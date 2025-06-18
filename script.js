518caf281ae3522efd2e75234aa8df297f647314
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-06-18 20:35:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
-    //Write your code here
-
-
+    // Select all <strong> elements
+    const strongElements = document.querySelectorAll('strong');
+    
+    // Change their color to green
+    strongElements.forEach(function(element) {
+        element.style.color = 'rgb(0, 128, 0)';
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
-
+    // Select all <strong> elements
+    const strongElements = document.querySelectorAll('strong');
     
+    // Revert their color to black
+    strongElements.forEach(function(element) {
+        element.style.color = 'rgb(0, 0, 0)';
+    });
 }
